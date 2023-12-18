87dedeec80ae201efcf1b0604dbd10b6abd9e619
Igor Morev <morevin@pik.ru>
2023-12-18 17:24:51 +1000

    build: init taiga ui

--- apps/angular-slack/src/app/app.component.ts
+++ apps/angular-slack/src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'as-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+    providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
 })
 export class AppComponent {}
--- libs/auth/feature-auth/src/lib/auth/auth.component.ts
+++ libs/auth/feature-auth/src/lib/auth/auth.component.ts
@@ -1,12 +1,32 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+import { TuiLabelModule } from '@taiga-ui/core';
+import { TuiButtonModule } from '@taiga-ui/core';
+import { TuiInputModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'as-auth',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiButtonModule,
+    TuiLabelModule,
+  ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
-export class AuthComponent {}
+export class AuthComponent {
+  readonly loginForm = new FormGroup({
+    userName: new FormControl('', Validators.required),
+  });
+}
