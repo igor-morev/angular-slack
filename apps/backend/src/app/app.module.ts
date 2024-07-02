50561dbc162c26d86053f75303f2bca31675c404
Igor Morev <morevin@pik.ru>
2024-07-02 16:34:47 +1000

    feat: prepared mongo

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+import { getMongoConfig } from '../config/db-connect.config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Позволяет обратиться к env во всем приложении
+      envFilePath: 'envs/.backend.env', // Указываем путь до env файла
+    }),
+    MongooseModule.forRootAsync({
+      // Модуль для работы с mongo
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig, // добавляем созданную ранее функцию подключения к БД
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
